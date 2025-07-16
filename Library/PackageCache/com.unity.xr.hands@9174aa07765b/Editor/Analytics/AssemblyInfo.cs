5c860a59825df569d24713d0dfee0d5e46e3ec2a
Dominik Sprute <dominikleonsprute@MacBook-Air-von-Dominik-2.local>
2025-07-16 16:45:26 +0200

    Add correct .gitignore

--- Library/PackageCache/com.unity.xr.hands@9174aa07765b/Editor/Analytics/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.XR.Hands.Editor")]
-[assembly: InternalsVisibleTo("Unity.XR.Hands")]
-[assembly: InternalsVisibleTo("Unity.XR.Hands.Analytics.Hooks.Editor")]
--- Library/PackageCache/com.unity.xr.hands@9174aa07765b/Editor/ProjectValidation/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.XR.Hands.Samples.Gestures.Editor")]
-[assembly: InternalsVisibleTo("Unity.XR.Hands.Samples.XRHandCapture.Editor")]
